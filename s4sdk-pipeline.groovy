dc1b3a4c6d853a8fa70f53ffbed04828f4536b8f
Daniel Kurzynski <daniel.kurzynski@sap.com>
2017-09-13 14:58:42 +0200

    First pipeline version

---
+++ s4sdk-pipeline.groovy
@@ -0,0 +1,73 @@
+#!/usr/bin/env groovy
+
+final def pipelineSdkVersion = 'master'
+
+pipeline {
+    agent any
+    options {
+        timeout(time: 30, unit: 'MINUTES')
+        timestamps()
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+    }
+    stages {
+        stage('Init') {
+            steps {
+                library "s4sdk-pipeline-library@${pipelineSdkVersion}"
+                initS4SdkPipeline script: this
+            }
+        }
+
+        stage('Build') {
+            steps {
+                parallel(
+                        "Backend": {
+                            node { stageBuildBackend script: this }
+                        },
+                        "Frontend":{
+                            node { stageBuildFrontend script: this }
+                        }
+                        )
+            }
+        }
+
+        stage('Local Tests') {
+            steps {
+                parallel (
+                        "Static Code Checks": {
+                            node { stageStaticCodeChecks script: this }
+                        },
+                        "Backend Unit Tests": {
+                            node { stageUnitTests script: this }
+                        },
+                        "Backend Integration Tests": {
+                            node { stageIntegrationTests script: this }
+                        },
+                        "Frontend Unit Tests": {
+                            node { stageFrontendUnitTests script: this }
+                        }
+                        )
+            }
+        }
+
+        stage('Remote Tests') {
+            steps {
+                parallel(
+                        "End to end Tests": {
+                            node { stageEndToEndTests script: this }
+                        },
+                        "Performance Tests": {
+                            node { stagePerformanceTests script: this }
+                        }
+                        )
+            }
+        }
+        stage('Quality Checks') {
+            steps { stageS4SdkQualityChecks script: this }
+        }
+
+        stage('Production Deployment') {
+            steps { stageProductionDeployment script: this }
+        }
+    }
+    post { failure { deleteDir() } }
+}
