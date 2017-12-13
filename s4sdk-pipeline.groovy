782c040509e2e7d0a1f95a9578afd55cd7efbbb8
Florian Wilhelm <florian.wilhelm02@sap.com>
2017-12-13 11:21:11 +0100

    Update the pipeline
    Changes include:
    
    * Do not block an executor for pipeline
    * Allow to send email after the build (post action)
    * Increase Timeout for builds to 120 minutes

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -1,19 +1,23 @@
 #!/usr/bin/env groovy
 
 final def pipelineSdkVersion = 'master'
-def stageConfig = [:]
+
 pipeline {
-    agent any
+    agent none
     options {
-        timeout(time: 60, unit: 'MINUTES')
+        timeout(time: 120, unit: 'MINUTES')
         timestamps()
         buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        skipDefaultCheckout()
     }
     stages {
         stage('Init') {
             steps {
                 library "s4sdk-pipeline-library@${pipelineSdkVersion}"
-                script { stageConfig = initS4SdkPipeline script: this }
+                node('') {
+                    checkout scm
+                    initS4SdkPipeline script: this
+                }
             }
         }
 
@@ -21,7 +25,7 @@ pipeline {
             parallel {
                 stage("Backend") { steps { node('') { stageBuildBackend script: this } } }
                 stage("Frontend") {
-                    when { expression { stageConfig.FRONT_END_BUILD } }
+                    when { expression { pipelineEnvironment.skipConfiguration.FRONT_END_BUILD } }
                     steps { node('') { stageBuildFrontend script: this } }
                 }
             }
@@ -33,21 +37,21 @@ pipeline {
                 stage("Backend Unit Tests") { steps { node('') { stageUnitTests script: this } } }
                 stage("Backend Integration Tests") { steps { node('') { stageIntegrationTests script: this } } }
                 stage("Frontend Unit Tests") {
-                    when { expression { stageConfig.FRONT_END_TESTS } }
+                    when { expression { pipelineEnvironment.skipConfiguration.FRONT_END_TESTS } }
                     steps { node('') { stageFrontendUnitTests script: this } }
                 }
             }
         }
 
         stage('Remote Tests') {
-            when { expression { stageConfig.REMOTE_TESTS } }
+            when { expression { pipelineEnvironment.skipConfiguration.REMOTE_TESTS } }
             parallel {
                 stage("End to End Tests") {
-                    when { expression { stageConfig.E2E_TESTS } }
+                    when { expression { pipelineEnvironment.skipConfiguration.E2E_TESTS } }
                     steps { node('') { stageEndToEndTests script: this } }
                 }
                 stage("Performance Tests") {
-                    when { expression { stageConfig.PERFORMANCE_TESTS } }
+                    when { expression { pipelineEnvironment.skipConfiguration.PERFORMANCE_TESTS } }
                     steps { node('') { stagePerformanceTests script: this } }
                 }
             }
@@ -58,18 +62,18 @@ pipeline {
         }
 
         stage('Security Checks') {
-            when { expression { stageConfig.SECURITY_CHECKS } }
+            when { expression { pipelineEnvironment.skipConfiguration.SECURITY_CHECKS } }
             parallel {
                 stage("Checkmarx Scan") {
-                    when { expression { stageConfig.CHECKMARX_SCAN } }
+                    when { expression { pipelineEnvironment.skipConfiguration.CHECKMARX_SCAN } }
                     steps { node('') { stageCheckmarxScan script: this } }
                 }
                 stage("WhiteSource Scan") {
-                    when { expression { stageConfig.WHITESOURCE_SCAN } }
+                    when { expression { pipelineEnvironment.skipConfiguration.WHITESOURCE_SCAN } }
                     steps { node('') { stageWhitesourceScan script: this } }
                 }
                 stage("Node Security Platform Scan") {
-                    when { expression { stageConfig.NODE_SECURITY_SCAN } }
+                    when { expression { pipelineEnvironment.skipConfiguration.NODE_SECURITY_SCAN } }
                     steps { node('') { stageNodeSecurityPlatform script: this } }
                 }
             }
@@ -79,16 +83,25 @@ pipeline {
         stage('Deployment') {
             parallel {
                 stage('Production Deployment') {
-                    when { expression { stageConfig.PRODUCTION_DEPLOYMENT } }
+                    when { expression { pipelineEnvironment.skipConfiguration.PRODUCTION_DEPLOYMENT } }
                     steps { node('') { stageProductionDeployment script: this } }
                 }
 
                 stage('Artifact Deployment') {
-                    when { expression { stageConfig.ARTIFACT_DEPLOYMENT } }
+                    when { expression { pipelineEnvironment.skipConfiguration.ARTIFACT_DEPLOYMENT } }
                     steps { node('') { stageArtifactDeployment script: this } }
                 }
             }
         }
     }
-    post { failure { deleteDir() } }
+    post {
+        always{
+            script{
+                if(pipelineEnvironment.skipConfiguration.SEND_NOTIFICATION){
+                    postActionSendNotification script: this
+                }
+            }
+        }
+        failure { deleteDir() }
+    }
 }
