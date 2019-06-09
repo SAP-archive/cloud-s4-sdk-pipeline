1de7d9f713c59bb21a473e75696e44718d913634
Ramachandra Kamath Arbettu <rkamath3@users.noreply.github.com>
2019-06-09 15:06:37 +0200

    Update s4sdk-pipeline.groovy

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -1,12 +1,9 @@
 #!/usr/bin/env groovy
 
-final def pipelineSdkVersion = 'master'
+final def pipelineSdkVersion = 'v17'
 
 pipeline {
     agent any
-     environment {
-        ON_K8S = 'true'
-    }
     options {
         timeout(time: 120, unit: 'MINUTES')
        // timestamps()
