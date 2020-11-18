0793f3377926422689898a4d4e302c4097e2f92d
s4sdkJenkinsBot <s4sdk@sap.com>
2020-11-18 07:26:40 +0000

    Set release version v45

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-final def pipelineSdkVersion = 'master'
+final def pipelineSdkVersion = 'v45'
 
 library "s4sdk-pipeline-library@${pipelineSdkVersion}"
 cloudSdkPipeline(script: this)
