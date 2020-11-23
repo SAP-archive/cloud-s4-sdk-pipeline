4790bed4dd86ff0436502594bdde695c6c59fc59
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-11-23 15:11:22 +0100

    test

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -1,9 +1,8 @@
-node {
-    error '''
-    The SAP Cloud SDK Pipeline has been merged into General Purpose Pipeline of project "Piper".
 
-    The last available version is https://github.com/SAP/cloud-s4-sdk-pipeline/releases/tag/v45
+error '''
+The SAP Cloud SDK Pipeline has been merged into General Purpose Pipeline of project "Piper".
 
-    Please see https://github.com/SAP/cloud-s4-sdk-pipeline/blob/master/gpp-guide.md for details on how to switch to General Purpose Pipeline.
-    '''
-}
+The last available version is https://github.com/SAP/cloud-s4-sdk-pipeline/releases/tag/v45
+
+Please see https://github.com/SAP/cloud-s4-sdk-pipeline/blob/master/gpp-guide.md for details on how to switch to General Purpose Pipeline.
+'''
