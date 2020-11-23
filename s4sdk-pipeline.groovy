5fd7e58422e894482c72bb3479cfb5c1572a3448
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-11-23 13:56:57 +0100

    reword

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -1,3 +1,7 @@
 node {
-    error "not working anymore"
+    error '''
+    The SAP Cloud SDK Pipeline has been merged into General Purpose Pipeline of project "Piper".
+
+    Please see https://github.com/SAP/cloud-s4-sdk-pipeline/blob/master/gpp-guide.md for details on how to switch to General Purpose Pipeline.
+    '''
 }
